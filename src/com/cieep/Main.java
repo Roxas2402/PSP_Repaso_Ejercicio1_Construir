65a45320fa47ee3d651a145818c461a90e010fca
Roxas2402 <98048769+Roxas2402@users.noreply.github.com>
2022-11-16 12:00:33 +0100

    h

--- src/com/cieep/Main.java
+++ src/com/cieep/Main.java
@@ -1,5 +1,16 @@
+package com.cieep;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        StringBuilder frase = new StringBuilder();
+        String palabra;
+
+        while (!(palabra = sc.nextLine()).equalsIgnoreCase("FIN")) {
+            frase.append(palabra).append(" ");
+        }
+        System.out.println(frase.toString().trim());
     }
 }
